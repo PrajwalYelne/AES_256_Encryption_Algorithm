7409e0e754bc38d54709557de567071e3d28ea69
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-01-10 15:02:13 +0530

    keyExpansion

--- Encryption/keyExpansion.v
+++ Encryption/keyExpansion.v
@@ -20,3 +20,9 @@ module keyExpansion(rc,key,keyout);
        assign keyout[191:160] = w0 ^ tem ^ rcon(rc)^ w1 ^ w2;
        assign keyout[159:128]  = w0 ^ tem ^ rcon(rc)^ w1 ^ w2 ^ w3;
        
+       assign t1 = w0 ^ tem ^ rcon(rc)^ w1 ^ w2 ^ w3;
+
+       assign keyout[127:96]=t2^w4;
+       assign keyout[95:64]=t2^w4^w5;
+       assign keyout[63:32]=t2^w4^w5^w6;
+       assign keyout[31:0]=t2^w4^w5^w6^w7;
