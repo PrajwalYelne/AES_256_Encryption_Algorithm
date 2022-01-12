65257e0491d850023e901afad08e62c8fe446139
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-01-12 11:20:33 +0530

    keyExpansion

--- Encryption/keyExpansion.v
+++ Encryption/keyExpansion.v
@@ -45,3 +45,15 @@ module keyExpansion(rc,key,keyout);
          4'h2: rcon=32'h04_00_00_00;
          4'h3: rcon=32'h08_00_00_00;
          4'h4: rcon=32'h10_00_00_00;
+         4'h5: rcon=32'h20_00_00_00;
+         4'h6: rcon=32'h40_00_00_00;
+         4'h7: rcon=32'h80_00_00_00;
+         4'h8: rcon=32'h1b_00_00_00;
+         4'h9: rcon=32'h36_00_00_00;
+         default: rcon=32'h00_00_00_00;
+       endcase
+
+     endfunction
+
+endmodule
+
