4f012edf3bf5837f57948b31f907703ea72643a5
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-01-28 19:28:51 +0530

    cipherTest

--- Encryption/cipherTest.v
+++ Encryption/cipherTest.v
@@ -4,3 +4,10 @@ module stupidciphertest;
     reg [255:0] key;
     reg [127:0] datain;
     wire [127:0] dataout;
+    
+    aescipher DUT (.clk(clk),.key(key),.datain(datain),.dataout(dataout));
+
+    initial 
+        begin
+        #10 datain=128'h00112233445566778899aabbccddeeff; key=256'h000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f;
+        end
