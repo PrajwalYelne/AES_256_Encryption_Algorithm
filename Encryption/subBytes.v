57a255c6209dda30394e40affdb73e7b978ec380
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-02-03 19:24:27 +0530

    subBytes

--- Encryption/subBytes.v
+++ Encryption/subBytes.v
@@ -8,4 +8,7 @@ module subbytes(data,sb);
          sbox q2( .a(data[111:104]),.c(sb[111:104]) );
          sbox q3( .a(data[103:96]),.c(sb[103:96]) );
   
-         
+         sbox q4( .a(data[95:88]),.c(sb[95:88]) );
+         sbox q5( .a(data[87:80]),.c(sb[87:80]) );
+         sbox q6( .a(data[79:72]),.c(sb[79:72]) );
+         sbox q7( .a(data[71:64]),.c(sb[71:64]) );
