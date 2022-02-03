4987c12e8b01fedd13615da7debccfc1f05354de
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-02-03 19:21:28 +0530

    keyTest

--- Encryption/keyTest.v
+++ Encryption/keyTest.v
@@ -10,3 +10,10 @@ module stupidKeyTest;
         begin
          #10 rc =4'b0010; key=256'hd59aecb85bf3c917fee94248de8ebe96b5a9328a2678a647983122292f6c79b3;
         end
+    
+    initial
+        begin
+            $monitor($time,"key=%h,keyout=%h",key,keyout); 
+        end
+
+endmodule
