d7fd5c39ad36d05d54d9c40a74e17ac1ec418fad
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-02-03 19:20:17 +0530

    keyTest

--- Encryption/keyTest.v
+++ Encryption/keyTest.v
@@ -5,3 +5,8 @@ module stupidKeyTest;
     wire [255:0] keyout;
     
     keyExpansion DUT (.rc(rc),.key(key),.keyout(keyout))
+    
+    initial 
+        begin
+         #10 rc =4'b0010; key=256'hd59aecb85bf3c917fee94248de8ebe96b5a9328a2678a647983122292f6c79b3;
+        end
