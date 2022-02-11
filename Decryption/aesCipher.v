59eb313b8d87504da674e9dd3eb8329527bd77dd
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-02-11 21:03:09 +0530

    aesCipher

--- Decryption/aesCipher.v
+++ Decryption/aesCipher.v
@@ -4,3 +4,10 @@ module aesdecipher(clk,datain,key,dataout);
     input [127:0] datain;
     input [255:0] key;
     output[127:0] dataout;
+    
+    wire [255:0] keyout1,keyout2,keyout3,keyout4,keyout5,keyout6,keyout7;
+    wire [127:0] key1,key2,key3,key4,key5,key6,key7,key8,key9,key10,key11,key12,key13,key14,key15;
+
+    keyExpansion k1(.rc(4'b0000),.key(key),.keyout(keyout1));
+    keyExpansion k2(.rc(4'b0001),.key(keyout1),.keyout(keyout2));
+    keyExpansion k3(.rc(4'b0010),.key(keyout2),.keyout(keyout3));
