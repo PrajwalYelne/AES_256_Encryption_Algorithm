f2a091c8b3b5f365eba62634dc60507bebcafa2d
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-02-19 13:18:37 +0530

    InverseMixColumn

--- Decryption/InverseMixColumn.v
+++ Decryption/InverseMixColumn.v
@@ -1,3 +1,7 @@
 module InverseMixColumn(a,mcl);
   input [127:0] a;
   output [127:0] mcl;
+  
+  MixColumnHelper j1(.rc(a[127:96]),.mcl(mcl[127:96]));
+  MixColumnHelper j2(.rc(a[95:64]),.mcl(mcl[95:64]));
+
