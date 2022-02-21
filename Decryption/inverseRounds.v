be0cad82f3a619971b043eb9f7ef80ffc553d975
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-02-21 10:47:01 +0530

    inverseRounds

--- Decryption/inverseRounds.v
+++ Decryption/inverseRounds.v
@@ -5,3 +5,7 @@ module InverseRounds(clk,data,keyin,rndout);
   output [127:0]rndout;
 
   wire [127:0] sb,sr,mcl;
+  
+  InverseShiftRow t2(.sb(data),.sr(sb));
+  InverseSubByte t1(.data(sb),.sb(sr));
+  assign mcl = sr^ keyin;
