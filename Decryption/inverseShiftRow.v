b5f33274834720b3c5c699239cfe6a65edd8fa61
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-03-16 10:19:27 +0530

    inverseShiftRow

--- Decryption/inverseShiftRow.v
+++ Decryption/inverseShiftRow.v
@@ -2,3 +2,8 @@ module InverseShiftRow(sb,sr);
 
   input [127:0] sb;
   output [127:0] sr;
+  
+  assign         sr[127:120]= sb[127:120];  
+  assign         sr[87:80]= sb[119:112];
+  assign          sr[47:40]= sb[111:104];
+  assign         sr[7:0]= sb[103:96];
