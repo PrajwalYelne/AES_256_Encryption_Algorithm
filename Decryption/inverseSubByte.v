3008924107d447d153d2078d40f68c9e19ebc3f5
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-03-26 11:18:35 +0530

    inverseSubByte

--- Decryption/inverseSubByte.v
+++ Decryption/inverseSubByte.v
@@ -8,4 +8,7 @@ module InverseSubByte(data,sb);
      InverseSbox q2( .a(data[111:104]),.c(sb[111:104]) );
      InverseSbox q3( .a(data[103:96]),.c(sb[103:96]) );
      
-     
+     InverseSbox q4( .a(data[95:88]),.c(sb[95:88]) );
+     InverseSbox q5( .a(data[87:80]),.c(sb[87:80]) );
+     InverseSbox q6( .a(data[79:72]),.c(sb[79:72]) );
+     InverseSbox q7( .a(data[71:64]),.c(sb[71:64]) );
