b93f80a0cce509bb0b43c3661faf8841c4d476c0
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-04-22 20:48:27 +0530

    mixColumnHelper

--- Decryption/mixColumnHelper.v
+++ Decryption/mixColumnHelper.v
@@ -2,7 +2,11 @@ module MixColumnHelper(rc,mcl);
   input [31:0] rc;
   output [31:0] mcl;
   
-    assign mcl[31:24] =  mule(rc[31:24])^mulb(rc[23:16])^muld(rc[15:8])^mul9(rc[7:0]);
-    assign mcl[23:16] =  mul9(rc[31:24])^mule(rc[23:16])^mulb(rc[15:8])^muld(rc[7:0]);
-    assign mcl[15:8] =  muld(rc[31:24])^mul9(rc[23:16])^mule(rc[15:8])^mulb(rc[7:0]);
-    assign mcl[7:0] =  mulb(rc[31:24])^muld(rc[23:16])^mul9(rc[15:8])^mule(rc[7:0]);
+      assign mcl[31:24] =  mule(rc[31:24])^mulb(rc[23:16])^muld(rc[15:8])^mul9(rc[7:0]);
+      assign mcl[23:16] =  mul9(rc[31:24])^mule(rc[23:16])^mulb(rc[15:8])^muld(rc[7:0]);
+      assign mcl[15:8] =  muld(rc[31:24])^mul9(rc[23:16])^mule(rc[15:8])^mulb(rc[7:0]);
+      assign mcl[7:0] =  mulb(rc[31:24])^muld(rc[23:16])^mul9(rc[15:8])^mule(rc[7:0]);
+  
+  function [7:0]	mul9;
+      input	[7:0]	rc;
+      case(rc)	
