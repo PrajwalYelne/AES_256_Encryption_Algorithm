23d9d5dc3ccd42b2b47f32d70d9a8d469c36f4d4
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2022-08-13 09:32:06 +0530

    Update decipherTest.v

--- Decryption/decipherTest.v
+++ Decryption/decipherTest.v
@@ -6,3 +6,9 @@ reg [127:0] datain;
 wire [127:0] dataout;
 
 aesdecipher DUT (.clk(clk),.key(key),.datain(datain),.dataout(dataout));
+
+initial
+  begin
+    #10 datain=128'h8ea2b7ca516745bfeafc49904b496089; key=256'h000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f;
+  end
+  
